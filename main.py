90fe0eeecd89a1d4cdfc0a0169e8352b44fea885
jacobwisniewski <jacobwisniewskim@gmail.com>
2019-05-17 10:42:12 +1000

    Added combine clip function

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 from dotenv import load_dotenv
 import os
-from datetime import datetime, timezone, timedelta
+from os import listdir
+from datetime import datetime, timezone, timedelta, date
 import requests
 import youtube_dl
+import ffmpeg
+
 
 # Loads the .env file
 load_dotenv()  # Get environment variables with VARIABLE = os.getenv("VARIABLE")
 
+# Todo: Add thumbnail generator
+# Todo: Add title generator, description generator
+# Todo: needs an ffmpeg script to combine the clips together
+
+def combine_clips():
+    x = datetime.now()
+    todays_date = x.strftime("%d-%m-%Y")
+    stream_list = []
+    for file in listdir(f'/home/jacob/files/twitch/{todays_date}'):
+        stream_list.append(ffmpeg.input(f'/home/jacob/files/twitch/{todays_date}/{file}'))
+    
+    ffmpeg.concat(*stream_list).output('output.mp4').run()
+
+
 
 def download_clips():
     top_clips = get_top_clips()
+    x = datetime.now()
+    todays_date = x.strftime("%d-%m-%Y")
+    ydl_opts = {
+        'outtmpl': f'~/files/twitch/{todays_date}/%(title)s',
+        'format': '720'
+    }
+
     for clip in top_clips:
-        with youtube_dl.YoutubeDL() as ydl:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            # Todo: figure out how to name files specific names
             ydl.download([clip['url']])
 
 
@@ -43,4 +68,4 @@ def get_top_clips():
 
 
 if __name__ == '__main__':
-    download_clips()
+    combine_clips()
